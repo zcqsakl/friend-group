b70e64a51afa74fdb8184a39462d5a0a89bc8f85
Anezka Klustova <ane.klustova.16@ucl.ac.uk>
2019-10-17 11:46:55 +0100

    Added dictionary of people with relation  + info

--- group.py
+++ group.py
@@ -2,4 +2,25 @@
 
 # Your code to go here...
 
-my_group =
+my_group = { 'Jill': { 'age': 26, 
+                       'job' : ['biologist'],
+                        'relation' : {'Zalika' : ['Friend'],
+                                      'John' : ['Partner'],}
+                     },
+            'Zalika' : { 'age': 28,
+                         'job' : ['artist'],
+                         'relation' : { 'Jill' :['Friend'],
+                                      'Nash' : ['Tenant']}
+                       },
+            'John' : {'age' : 27,
+                      'job' : ['writer'],
+                      'relation': {'Jill' : ['Partner'],
+                                   'Nash' : ['Cousin']}
+                     },
+            'Nash' : {'age' : 34,
+                      'job' : ['chef'],
+                      'relation' : { 'John' : ['Cousin'],
+                                    'Zalika' : ['Landlord']}
+                     }
+           }
+                 
